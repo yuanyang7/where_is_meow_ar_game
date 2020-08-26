ef72504fd541288f3dc02b825a122675922719ae
Yuan Yang <yangyuan95@gmail.com>
2020-08-25 22:34:27 -0700

    Testing Build

---
+++ build/precache-manifest.b86c59b405c1dacc3aa5e72898eb1dee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e22eec1b0c20ee01ab86d417115c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a0c6cc8b8fe60336760",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c173a7772cb00453ffdb",
+    "url": "/static/js/2.80482b71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80482b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0c6cc8b8fe60336760",
+    "url": "/static/js/main.76284053.chunk.js"
+  },
+  {
+    "revision": "0aa85706556652f9d5bc",
+    "url": "/static/js/runtime-main.0fdd0a66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
